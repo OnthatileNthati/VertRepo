87f2f4b019f2dd63d201f74b6e7504dd975e01ae
Onthatile Mosoma <onthatilenthati408@gmail.com>
2026-04-28 21:39:39 +0200

    Fix typo in fetch function for business data initialization

--- listing.js
+++ listing.js
@@ -140,7 +140,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //===FETCH BUSINESS DATA===
 async function init() {
-    onse = await fetch ('./businesses.json');
+    const response = await fetch ('./businesses.json');
     const businesses = await response.json();
 
     //==GET ID AND URL==
