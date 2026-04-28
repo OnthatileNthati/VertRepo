fa3e2c0c695cd041d7504c957626da728526de57
Onthatile Mosoma <onthatilenthati408@gmail.com>
2026-04-28 21:19:39 +0200

    Fix fetch path for business data in listing and search scripts

--- listing.js
+++ listing.js
@@ -140,7 +140,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //===FETCH BUSINESS DATA===
 async function init() {
-    const response = await fetch ('business.json');
+    const response = await fetch ('./business.json');
     const businesses = await response.json();
 
     //==GET ID AND URL==
